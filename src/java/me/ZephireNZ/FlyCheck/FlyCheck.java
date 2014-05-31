d167f690d287c7938afe2fba8f5c21f51b91958a
ZephireNZ <brynleymcdonald@gmail.com>
2014-06-01 01:08:38 +1200

    Update to UUIDs with ZephCore, and adapt to new FlyPotions API

--- src/java/me/ZephireNZ/FlyCheck/FlyCheck.java
+++ src/java/me/ZephireNZ/FlyCheck/FlyCheck.java
@@ -1,8 +1,10 @@
 package me.ZephireNZ.FlyCheck;
 
 import me.ZephireNZ.FlyPotions.FlyPotions;
+import nz.co.noirland.zephcore.Util;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -32,9 +34,9 @@ public class FlyCheck extends JavaPlugin {
         if(cmd.getName().equalsIgnoreCase("flycheck")) {
             if(sender.hasPermission("flycheck.check")) {
                 if(args.length == 1) {
-                    Player player = getServer().getPlayer(args[0]);
-                    if(player != null) {
-                        checkPlayer(sender, player);
+                    OfflinePlayer player = Util.player(args[0]);
+                    if(player.isOnline()) {
+                        checkPlayer(sender, player.getPlayer());
                         return true;
                     }
                 }
@@ -47,27 +49,19 @@ public class FlyCheck extends JavaPlugin {
     }
 
     public void checkPlayer(CommandSender sender, Player player) {
-            boolean creative = player.getGameMode().equals(GameMode.CREATIVE);
-            boolean flypotion = false;
-            boolean flyPotionsPlugin = flyPotions != null;
-            boolean fly = false;
-            if(flyPotionsPlugin) {
-                if(flyPotions.flying.contains(player.getName())) {
-                    flypotion = true;
-                }
-            }
-            if(!player.getGameMode().equals(GameMode.CREATIVE) && player.getAllowFlight()) {
-                if(!(flyPotionsPlugin && flypotion)) {
-                    fly = true;
-                }
-            }
-            String             sendString  = player.getName() + ChatColor.RESET + "";
-            if(creative)       sendString += ChatColor.GREEN + " can fly "   + ChatColor.RESET + "because of creative.";
-            else if(flypotion) sendString += ChatColor.GREEN + " can fly "   + ChatColor.RESET + "because of fly potions.";
-            else if(fly)       sendString += ChatColor.GREEN + " can fly "   + ChatColor.RESET + "because of another plugin or /fly.";
-            else               sendString += ChatColor.RED   + " CANNOT fly" + ChatColor.RESET + ".";
+        String sendString  = player.getName() + ChatColor.RESET + "";
 
-            sender.sendMessage(sendString);
+        if(player.getGameMode().equals(GameMode.CREATIVE)) {
+            sendString += sendString += ChatColor.GREEN + " can fly "   + ChatColor.RESET + "because of creative.";
+        } else if(flyPotions != null && flyPotions.isUsingPotion(player.getUniqueId())) {
+            sendString += ChatColor.GREEN + " can fly " + ChatColor.RESET + "because of fly potions.";
+        } else if(player.getAllowFlight()) {
+            sendString += ChatColor.GREEN + " can fly " + ChatColor.RESET + "because of another plugin or /fly.";
+        } else {
+            sendString += ChatColor.RED   + " CANNOT fly" + ChatColor.RESET + ".";
+        }
+
+        sender.sendMessage(sendString);
 
     }
 }
